562cd547b02c08d3c4b35b30fe78eca14011dbc8
fromero9472 <federico.romero9472@gmail.com>
2025-01-16 20:07:49 +0100

    sonarCorreccionException

--- src/main/java/com/inditex/domain/exception/DecryptionException.java
+++ src/main/java/com/inditex/domain/exception/DecryptionException.java
@@ -4,4 +4,8 @@ public class DecryptionException extends RuntimeException {
     public DecryptionException(String message) {
         super(message);
     }
+
+    public DecryptionException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
